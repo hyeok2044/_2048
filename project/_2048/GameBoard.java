836a2a95b00c20134bf0443844d6083c7e17d7a7
이동혁 <dlehdhyeok20@naver.com>
2022-06-12 05:45:14 +0000

    first commit

---
+++ project/_2048/GameBoard.java
@@ -0,0 +1,10 @@
+package project._2048;
+
+public class GameBoard {
+    private int dimension_w, dimension_h;
+    public GameBoard(int w, int h){
+        dimension_h = h;
+        dimension_w = w;
+    }
+    
+}
---
+++ project/_2048/GameCell.java
@@ -0,0 +1,13 @@
+package project._2048;
+
+import java.math.BigInteger;
+
+public class GameCell {
+    private int location_x, location_y;
+    public GameCell(int x, int y){
+        this.location_x = x;
+        this.location_y = y;
+    }
+    
+    public BigInteger 
+}
